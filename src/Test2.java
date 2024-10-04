40f491262050181d13a3d5b481c60764cac93b54
ar-vivek96 <vivekyadhav96@gmail.com>
2024-10-04 12:48:05 +0530

    added Additional checks..

--- src/Test2.java
+++ src/Test2.java
@@ -2,7 +2,11 @@
 public class Test2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String hungry ="yes";
+		if(hungry=="yes"|| hungry=="y"||hungry=="Y")
+			System.out.println("Eat something");
+		else
+			System.out.println("Do Work");
 
 	}
 
